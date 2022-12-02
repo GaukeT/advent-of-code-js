5d7dfc71d74b779dbb448bc89b46ba1c19ddd0fe
GaukeT <gauket.dev@gmail.com>
2022-12-02 12:10:43 +0100

    empty day 3 + impl day 2

--- js/day2.js
+++ js/day2.js
@@ -1,18 +1,60 @@
 // format raw data from input file
 export function formatRawData(rawData) {
-    return rawData;
+    return rawData.split("\n");
 }
 
 // part 1
 export function solve1(input) {
     var result = 0;    
 
+    input.forEach(game => {
+        result += possibilities[game];
+    });
+
     return result;
 }
 
 // part 2
 export function solve2(input) {
-    var result = 0;
+    var result = 0;    
+
+    input.forEach(game => {
+        result += possibilities2[game];
+    });
 
     return result;
 }    
+
+// A (Rock)     + 1   
+// B (Paper)    + 2
+// C (Scissors) + 3
+
+// X (Rock)     
+// Y (Paper)    
+// Z (Scissors) 
+var possibilities = {
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6
+}
+
+// X (Lose) + 0
+// Y (Draw) + 3
+// Z (Win)  + 6
+var possibilities2 = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7
+}
---
+++ js/day3.js
@@ -0,0 +1,18 @@
+// format raw data from input file
+export function formatRawData(rawData) {
+    return rawData;
+}
+
+// part 1
+export function solve1(input) {
+    var result = 0;    
+
+    return result;
+}
+
+// part 2
+export function solve2(input) {
+    var result = 0;
+
+    return result;
+}    
